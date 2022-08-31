5f32d72757a8372a87aad0f3924389470ce68212
Mihail A <mixadze1@bk.ru>
2022-08-31 11:11:16 +0300

    division of responsibility

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -8,37 +8,33 @@ using Anim;
 [RequireComponent (typeof(AnimationChange))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private LayerMask _aimLayerMask;
-
-    [SerializeField] private Transform _bulletTransform; 
-
     [SerializeField] private List<NavMeshPosition> _position;
-
-    [SerializeField] private float _distanceShoot;
-
-    [SerializeField, Range(0.2f,0.3f)] private float _prepareShoot = 0.25f;
-    [SerializeField,Range (0.45f,1f)] private float _timeToNextShoot = 0.45f;
 
     private NavMeshAgent _agent;
     private GameStarter _game;
-    private Bullet _bulletSave;
-    private Coroutine _coroutine;
     private AnimationChange _animationChange;
-    private Camera _camera;
-   
-    private int _countClick;
+    private PlayerShoot _playerShoot;
 
-    private bool _canShoot;
-    private bool _isShoot;
+    private int _countClick;
 
     public bool IsMoving;
 
+    public NavMeshPosition Position => _position[_countClick];
+
+    public AnimationChange AnimationChange => _animationChange;
+
     public void Initialize(GameStarter game, Camera camera)
     {
-        _camera = camera;
         _game = game;
+        GetNeedComponent();
+        _playerShoot.Initialize(this,camera,_game);
+    }
+
+    private void GetNeedComponent()
+    {
         _animationChange = GetComponent<AnimationChange>();
         _agent = GetComponent<NavMeshAgent>();
+        _playerShoot = GetComponent<PlayerShoot>();
     }
 
     private void Update()
@@ -51,26 +47,9 @@ public class Player : MonoBehaviour
             _countClick++;
             StartMovingPos();
         }
-
-        if (IsCanShoot())
-        {
-            Shoot(GetRay());
-        }
         CheckDestination();
     }
 
-    private Ray GetRay()
-    {
-        return _camera.ScreenPointToRay(Input.mousePosition);
-    }
-
-    private bool IsCanShoot()
-    {
-        if (Input.GetMouseButtonDown(0) && _canShoot && !_isShoot && _position[_countClick].
-            Enemies.Count > 0)
-            return true;
-        return false;
-    }
 
     private bool IsNextPosition()
     {
@@ -102,7 +81,7 @@ public class Player : MonoBehaviour
             }
             else
             {
-                _canShoot = false;
+                _playerShoot.CanShoot = false;
             }
         }
     }
@@ -110,82 +89,12 @@ public class Player : MonoBehaviour
     private void CheckEnemies()
     {
         if (_position[_countClick]. Enemies.Count > 0)
-            _canShoot = true;
+            _playerShoot.CanShoot = true;
     }
 
     private void CheckFinish()
     {
         if (_position[_countClick].IsFinish)
             _game.RestartGame();
-    }
-
-    private void Shoot(Ray ray)
-    {
-        if (_coroutine != null)
-            StopCoroutine(_coroutine);
-        _coroutine = StartCoroutine(PrepareShoot(ray));
-    }
-
-    private IEnumerator PrepareShoot(Ray ray)
-    {
-        _animationChange.ChangeAnimationState(Anim.Player.SHOOT); 
-        _isShoot = true;
-        
-        yield return new WaitForSeconds(_prepareShoot);
-        InitializeBullet();
-
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, _distanceShoot, _aimLayerMask))
-        {
-            BulletTrajectory(hitInfo);
-            RotationPlayerToShoot(hitInfo);
-        }
-        yield return new WaitForSeconds(_timeToNextShoot);
-        _isShoot = false;
-    }
-
-    private void InitializeBullet()
-    {
-        Bullet bullet = _bulletSave = _game.Bullets.GetFreeElement();
-
-        ResetBullet(bullet);
-        ResetBulletRigidbody(bullet);
-        bullet.GetComponentInChildren<TrailRenderer>().Clear();
-        bullet.gameObject.SetActive(true);
-        bullet.Initialize();
-    }
-
-    private void ResetBullet(Bullet bullet)
-    {
-        bullet.transform.position = _bulletTransform.position;
-        bullet.transform.rotation = new Quaternion(0, 0, 0, 0);
-        bullet.Direction = Vector3.zero; 
-    }
-
-    private void ResetBulletRigidbody(Bullet bullet)
-    {
-        Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
-        rigidbody.velocity = Vector3.zero;
-        rigidbody.angularVelocity = Vector3.zero;
-    }
-
-    private void BulletTrajectory(RaycastHit hitInfo)
-    {
-        var destination = hitInfo.point;
-        var direction = (destination - _bulletSave.transform.position).normalized;
-        _bulletSave.Direction = direction;
-    }
-
-    private void RotationPlayerToShoot(RaycastHit hitInfo)
-    {
-        var destination = hitInfo.point;
-
-        destination.y = transform.position.y;
-        var direction = destination - transform.position; 
-         direction.Normalize();
-
-        Quaternion rotation =  Quaternion.LookRotation(direction, transform.position);
-        rotation.x = 0;
-        rotation.z = 0;
-        transform.rotation = rotation;
     }
 }
---
+++ Assets/_Scripts/PlayerShoot.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private LayerMask _aimLayerMask;
+    [SerializeField] private float _distanceShoot;
+    [SerializeField] private Transform _bulletTransform;
+    [SerializeField, Range(0.2f, 0.3f)] private float _prepareShoot = 0.25f;
+    [SerializeField, Range(0.45f, 1f)] private float _timeToNextShoot = 0.45f;
+
+    private Player _player;
+    private Bullet _bulletSave;
+    private Coroutine _coroutine;
+    private GameStarter _game;
+    private Camera _camera;
+
+    private bool _canShoot;
+    private bool _isShoot;
+
+    public bool CanShoot { get => _canShoot; set => _canShoot = value; }
+    public bool IsShoot { get => _isShoot; set => _isShoot = value; }
+
+    public void Initialize(Player player, Camera camera, GameStarter game)
+    {
+        _game = game;
+        _player = player;
+        _camera = camera;
+    }
+
+    private void Update()
+    {
+        if (IsCanShoot())
+        {
+            Shoot(GetRay());
+        }
+    }
+
+    private Ray GetRay()
+    {
+        return _camera.ScreenPointToRay(Input.mousePosition);
+    }
+
+    private void Shoot(Ray ray)
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+        _coroutine = StartCoroutine(PrepareShoot(ray));
+    }
+
+    private bool IsCanShoot()
+    {
+        if (Input.GetMouseButtonDown(0) && _canShoot && !_isShoot && _player.Position.
+            Enemies.Count > 0)
+            return true;
+        return false;
+    }
+
+
+    private IEnumerator PrepareShoot(Ray ray)
+    {
+        _player.AnimationChange.ChangeAnimationState(Anim.Player.SHOOT);
+        _isShoot = true;
+
+        yield return new WaitForSeconds(_prepareShoot);
+        InitializeBullet();
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, _distanceShoot, _aimLayerMask))
+        {
+            BulletTrajectory(hitInfo);
+            RotationPlayerToShoot(hitInfo);
+        }
+        yield return new WaitForSeconds(_timeToNextShoot);
+        _isShoot = false;
+    }
+
+    private void InitializeBullet()
+    {
+        Bullet bullet = _bulletSave = _game.Bullets.GetFreeElement();
+
+        ResetBullet(bullet);
+        ResetBulletRigidbody(bullet);
+        bullet.GetComponentInChildren<TrailRenderer>().Clear();
+        bullet.gameObject.SetActive(true);
+        bullet.Initialize();
+    }
+
+    private void ResetBullet(Bullet bullet)
+    {
+        bullet.transform.position = _bulletTransform.position;
+        bullet.transform.rotation = new Quaternion(0, 0, 0, 0);
+        bullet.Direction = Vector3.zero;
+    }
+
+    private void ResetBulletRigidbody(Bullet bullet)
+    {
+        Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+    }
+
+    private void BulletTrajectory(RaycastHit hitInfo)
+    {
+        var destination = hitInfo.point;
+        var direction = (destination - _bulletSave.transform.position).normalized;
+        _bulletSave.Direction = direction;
+    }
+
+    private void RotationPlayerToShoot(RaycastHit hitInfo)
+    {
+        var destination = hitInfo.point;
+
+        destination.y = transform.position.y;
+        var direction = destination - transform.position;
+        direction.Normalize();
+
+        Quaternion rotation = Quaternion.LookRotation(direction, transform.position);
+        rotation.x = 0;
+        rotation.z = 0;
+        transform.rotation = rotation;
+    }
+}
